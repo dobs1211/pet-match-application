dbdf8477b52fc6883048efea03c14098b5d03e9d
Olga Chavez <olgakatrin827@gmail.com>
2016-12-01 04:01:27 +0000

    scroll-added-tidied-up

---
+++ public/js/data.js
@@ -0,0 +1,70 @@
+//needs modified for dog match
+var quizData = [
+	{
+		prompt: 'Are you always on the go? Do you struggle to just sit still?',
+		answers: [
+			{text: 'Very much like me', score: 0},
+			{text: 'Somewhat like me', score: 50},
+			{text: 'Not like me at all', score: 100}
+		]
+	},
+	{
+		prompt: 'Are you a clean-freak, dust bunnies your nemisis?',
+		answers: [
+			{text: 'Very much like me', score: 100},
+			{text: 'Somewhat like me', score: 50},
+			{text: 'Not like me at all', score: 0}
+		]
+	},
+	{
+		prompt: 'Would you like your canine friend to be super smart and help you with your taxes?',
+		answers: [
+			{text: 'Very much like me', score: 0},
+			{text: 'Mostly like me', score: 25},
+			{text: 'Somewhat like me', score: 50},
+			{text: 'Not much like me', score: 75},
+			{text: 'Not like me at all', score: 100}
+		]
+	},
+	{
+		prompt: 'Do you want your dog to bark to alert you anything is amiss? Security is key',
+		answers: [
+			{text: 'Very much like me', score: 100},
+			{text: 'Mostly like me', score: 75},
+			{text: 'Somewhat like me', score: 50},
+			{text: 'Not much like me', score: 25},
+			{text: 'Not like me at all', score: 0}
+		]
+	},
+	{
+		prompt: 'What size home do you have?',
+		answers: [
+			{text: 'Very much like me', score: 0},
+			{text: 'Mostly like me', score: 25},
+			{text: 'Somewhat like me', score: 50},
+			{text: 'Not much like me', score: 75},
+			{text: 'Not like me at all', score: 100}
+		]
+	},
+	{
+		prompt: 'I have difficulty maintaining my focus on projects that take more than a few months to complete.',
+		answers: [
+			{text: 'Very much like me', score: 0},
+			{text: 'Mostly like me', score: 25},
+			{text: 'Somewhat like me', score: 50},
+			{text: 'Not much like me', score: 75},
+			{text: 'Not like me at all', score: 100}
+		]
+	},
+	{
+		prompt: 'Canines come in all sizes, from tea cup size to mini horse size...',
+		answers: [
+			{text: 'Very much like me', score: 100},
+			{text: 'Mostly like me', score: 75},
+			{text: 'Somewhat like me', score: 50},
+			{text: 'Not much like me', score: 25},
+			{text: 'Not like me at all', score: 0}
+		]
+	},
+	
+];
